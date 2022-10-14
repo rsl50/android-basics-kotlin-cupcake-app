ffd17c7fe14648a2dcc6b25887a9fb588cac672c
Robson Lima <rsl.robson@gmail.com>
2022-10-14 18:45:04 -0300

    Using Jetpack Navigation to control screen flow.

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -19,8 +19,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
 
 /**
@@ -57,7 +57,8 @@ class StartFragment : Fragment() {
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
      */
     fun orderCupcake(quantity: Int) {
-        Toast.makeText(activity, "Ordered $quantity cupcake(s)", Toast.LENGTH_SHORT).show()
+        // Navigate from start fragment to flavor fragment
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
     }
 
     /**
