1ee67af972725cc5c7d7b5b3a5983adf6e349cab
Robson Lima <rsl.robson@gmail.com>
2022-10-16 16:08:30 -0300

    Adding method to send order to another app in SummaryFragment.

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -15,6 +15,7 @@
  */
 package com.example.cupcake
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -68,7 +69,29 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
-        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+        // Construct the order summary text with information from the view model
+        val orderSummary = getString(
+            R.string.order_details,
+            sharedViewModel.quantity.value.toString(),
+            sharedViewModel.flavor.value.toString(),
+            sharedViewModel.date.value.toString(),
+            sharedViewModel.price.value.toString()
+        )
+
+        // Create an ACTION_SEND implicit intent with order details in the intent extras
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
+            .putExtra(Intent.EXTRA_TEXT, orderSummary)
+            //.putExtra(Intent.EXTRA_EMAIL, "someemail@email.com") // add the email recipient with
+            // intent extra
+
+        // Check if there's an app that can handle this intent before launching it
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            // Start a new activity with the given intent (this may open the share dialog on a
+            // device if multiple apps can handle this intent)
+            startActivity(intent)
+        }
     }
 
     /**
